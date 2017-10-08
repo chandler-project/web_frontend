796b9ab8802f36918b9825889f87fe9e068aa209
nvpmai95 <nvpmai95@gmail.com>
2017-10-09 03:25:06 +0700

    initialize

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    <button class="btn btn-default">Login with Facebook</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+  }
+}
+</script>
